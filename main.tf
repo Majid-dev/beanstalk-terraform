93882da8de1197f85d0b97c7b650a044b1924d50
Majid Karimi <majid0karimi@gmail.com>
2023-06-28 10:00:26 +0430

    add modules to main.tf file

---
+++ main.tf
@@ -0,0 +1,19 @@
+module "vpc" {
+  source                       = "./modules/vpc"
+  region                       = var.region
+  project_name                 = var.project_name
+  environment                  = var.environment
+  vpc_cidr                     = var.vpc_cidr
+  public_subnet_az1_cidr       = var.public_subnet_az1_cidr
+  public_subnet_az2_cidr       = var.public_subnet_az2_cidr
+  private_app_subnet_az1_cidr  = var.private_app_subnet_az1_cidr
+  private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
+  private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
+  private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
+}
+
+
+module "security-group" {
+  source = "./modules/security-group"
+  vpc_id = module.vpc.vpc_id
+}
